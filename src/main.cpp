29107a9541301c2d0beae1d9167c72ca47e8260d
Oderik <oderikmail-github@yahoo.de>
2021-04-15 00:04:04 +0200

    Experiment with iot framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include "LittleFS.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#include "WiFiManager.h"
+#include "webServer.h"
+#include "updater.h"
+#include "fetch.h"
+#include "configManager.h"
+#include "timeSync.h"
+
+void setup() 
+{
+    Serial.begin(115200);
+
+    LittleFS.begin();
+    GUI.begin();
+    configManager.begin();
+    WiFiManager.begin(configManager.data.projectName);
+    timeSync.begin();
+
+    Serial.println("Hello world");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+    //software interrupts
+    WiFiManager.loop();
+    updater.loop();
+    configManager.loop();
+
+    //your code here
 }
