c3df18868e035f924d9d6a314427fbf5c5936a5c
Oderik <oderikmail-github@yahoo.de>
2021-05-01 00:31:06 +0200

    Use LED ring

--- src/main.cpp
+++ src/main.cpp
@@ -12,31 +12,71 @@
 
 #include "Encoder.h"
 
+#include <FastLED.h>
+
+#define COLOR_ORDER GRB
+#define CHIPSET WS2812B
+#define NUM_LEDS 12
 
 Encoder encoder(D1, D2);
 
-void setup() 
+CRGB leds[NUM_LEDS];
+
+void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
+  Serial.println("Starting...");
 
-    LittleFS.begin();
-    GUI.begin();
-    configManager.begin();
-    WiFiManager.begin(configManager.data.projectName);
-    timeSync.begin(TZ_Europe_Berlin);
-    dash.begin(750);
+  LittleFS.begin();
+  GUI.begin();
+  configManager.begin();
+  WiFiManager.begin(configManager.data.projectName);
+  timeSync.begin(TZ_Europe_Berlin);
+  dash.begin(750);
 
-    Serial.println("Hello world");
+  FastLED.addLeds<CHIPSET, D7, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  Serial.println("Hello world");
 }
 
-void loop() 
+int encoderValue = 0;
+
+void loop()
 {
-    //software interrupts
-    WiFiManager.loop();
-    updater.loop();
-    configManager.loop();
-    dash.loop();
+  //software interrupts
+  WiFiManager.loop();
+  updater.loop();
+  configManager.loop();
+  dash.loop();
+  
+  if (FastLED.getBrightness() != configManager.data.Brightness) {
+    FastLED.setBrightness(configManager.data.Brightness);
+    FastLED.show();
+  }
 
-    dash.data.encoder = encoder.read();
+  int newEncoderValue = (encoder.read() + 2) / 4;
+  while (newEncoderValue < 0) {
+    //TODO: find better approach
+    newEncoderValue += NUM_LEDS;
+  }
 
+  if (newEncoderValue != encoderValue)
+  {
+    encoderValue = newEncoderValue;
+    dash.data.encoder = encoderValue;
+
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+      if (i == encoderValue % NUM_LEDS)
+      {
+        leds[i] = 0x00FFFFFF;
+      }
+      else
+      {
+        leds[i] = 0;
+      }
+    }
+
+    FastLED.show();
+  }
 }
